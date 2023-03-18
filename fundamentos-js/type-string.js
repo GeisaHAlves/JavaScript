8393956728a47f17c3fa48f5a3f7fb5c2092c899
Geisa Hermsdorff Alves <97129805+Geisalves54@users.noreply.github.com>
2023-03-18 10:59:20 -0300

    exemplos aula 1

---
+++ fundamentos-js/type-string.js
@@ -0,0 +1,13 @@
+const texto1 = "Hello, world!";
+const texto2 = 'Hello, world!';
+const senha = "senhaNaoMuitoSegura123!";
+const StringDeNumeros = "123566";
+
+const citacao = "My name is ";
+const meuNome = "Geisa";
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
