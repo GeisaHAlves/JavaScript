dd0f9146ad29eb1906933c0d391e7f6bd299a1b0
Geisa Hermsdorff Alves <97129805+Geisalves54@users.noreply.github.com>
2023-03-28 22:25:42 -0300

    JavaScript Objetos

---
+++ objetos-js/criando-objetos.js
@@ -0,0 +1,23 @@
+const pessoa = {
+    nome:"Peter Parker",
+    dataNascimento:"25/05/1989",
+    carteiraIdentidade:"999362-1",
+    email:"peterparker@email.com",
+    telefone:"+552711112222",
+    cidade:"Cariacica",
+    estado:"ES"
+ }
+
+ console.log(pessoa);
+
+ pessoa.cpf = "11111111122";
+
+ console.log(pessoa);
+
+ pessoa.endereco = "Rua dos Cravos, 126";
+
+ console.log(pessoa);
+
+ pessoa.celular = "22-999364747";
+
+ console.log(pessoa);
